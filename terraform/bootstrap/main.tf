8dbbf71cec8044dc0fb1dcef1dedc636e8b122ad
silviaistr52-collab <silvia.istr52@gmail.com>
2026-03-31 14:54:27 +0100

    added kms encryption for the logs bucket too

--- terraform/bootstrap/main.tf
+++ terraform/bootstrap/main.tf
@@ -145,7 +145,8 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "tfstate_logs" {
 
   rule {
     apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256"
+      sse_algorithm     = "aws:kms"
+      kms_master_key_id = aws_kms_key.tfstate.arn
     }
   }
 }
